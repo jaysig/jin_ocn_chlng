df4e6b4301afc4ec441c4807033015405a9f8838
Jaysig <jasontsigmon@gmail.com>
2018-02-28 13:38:22 -0600

    Fixed addPost action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,20 @@
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const CREATE_POST = 'CREATE_POST';
+// export const FETCH_POSTS = 'FETCH_POSTS';
+export const ADD_POST = 'ADD_POST';
 export const FETCH_POST = 'FETCH_POST';
 
-export function fetchPosts() {
-  const request = '';
+// export function fetchPosts() {
+//   const request = '';
+//
+//   return {
+//     type: FETCH_POSTS,
+//     payload: request,
+//   };
+// }
 
+export const addPost = text => {
+  console.log(text,'text');
   return {
-    type: FETCH_POSTS,
-    payload: request,
-  };
-}
-
-export function createPost(props) {
-  const request = '';
-
-  return {
-    type: CREATE_POST,
-    payload: request,
-  };
+    type: ADD_POST,
+    text
+  }
 }
